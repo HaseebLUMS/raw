cce2006466bff872bc113efa35b234517d683c9d
Muhammad Haseeb <haseeb.lums@gmail.com>
2019-04-16 13:03:30 +0500

    Update background.js

--- ShareWithCare/background.js
+++ ShareWithCare/background.js
@@ -1,3 +1,9 @@
+let prev_message = ''
 chrome.runtime.onMessage.addListener(function(response, sender, _){
-	console.log(response);
+	if(prev_message !== response)
+	{
+		prev_message = response; 
+		info = JSON.parse(response)
+		console.log("For ", info[2], "\nInformation: ", info[1])
+	}
 });
